7a363f35384eed17f8876eab8e099c91e828e7f3
Dinesh-Jakka <ddinesh.jakka@gmail.com>
2024-10-21 02:32:54 +0530

    updated projects section

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -3,37 +3,35 @@ import './index.css'; // Import your CSS for styling
 
 const projects = [
   {
-    date: 'Mar 2024',
-    title: 'Rock Paper Scissors Game',
-    description: 'Developed interactive Rock Paper Scissors game with modal rules display and dynamic scoring using React and Styled Components.',
-    imageSrc: '/images/rock_paper_scissors_game.png',
-    technologies: ['React JS', 'Node JS'],
-    link: 'https://example.com/rock-paper-scissors' // Sample link
-  },
-  {
-    date: 'Oct 2024',
-    title: 'Nxt Trendz: Fashion Retail Website',
-    description: 'Developed advanced e-commerce cart system with intuitive UX and robust management capabilities.',
-    imageSrc: '/images/nxt_trendz_fashion_retail_website.png',
-    technologies: ['React JS', 'Node JS'],
-    link: 'https://example.com/nxt-trendz' // Sample link
-  },
-  {
-    date: 'Mar 2024',
-    title: 'Nxtwatch: Video Streaming Platform',
-    description: 'Built Nxt Watch using React and Styled Components, showcasing responsive design, API-driven video content, authentication handling, and theme customization.',
-    imageSrc: '/images/nxtwatch__video_streaming_platform.png',
-    technologies: ['React JS', 'Node JS'],
-    link: 'https://example.com/nxt-watch' // Sample link
-  },
-  {
-    date: 'Mar 2024',
     title: 'Jobby: Video Streaming Platform',
     description: 'Built Nxt Watch using React and Styled Components, showcasing responsive design, API-driven video content, authentication handling, and theme customization.',
     imageSrc: '/images/jobby_sm.png',
     technologies: ['React JS', 'Node JS'],
     link: 'https://dineshjJobbyApp.ccbp.tech'
   },
+  {
+    title: 'Nxt Trendz: Fashion Retail Website',
+    description: 'Developed advanced e-commerce cart system with intuitive UX and robust management capabilities.',
+    imageSrc: '/images/nxt_trendz_fashion_retail_website.png',
+    technologies: ['React JS', 'Node JS'],
+    link: 'https://dineshjnxtrendz.ccbp.tech/'
+  },
+  {
+    title: 'Rock Paper Scissors Game',
+    description: 'Developed interactive Rock Paper Scissors game with modal rules display and dynamic scoring using React and Styled Components.',
+    imageSrc: '/images/rock_paper_scissors_game.png',
+    technologies: ['React JS', 'Node JS'],
+    link: 'https://example.com/rock-paper-scissors' 
+  },
+  
+  {
+    title: 'Nxtwatch: Video Streaming Platform',
+    description: 'Built Nxt Watch using React and Styled Components, showcasing responsive design, API-driven video content, authentication handling, and theme customization.',
+    imageSrc: '/images/nxtwatch__video_streaming_platform.png',
+    technologies: ['React JS', 'Node JS'],
+    link: 'https://dineshjnxtwatch.ccbp.tech/'
+  },
+  
 ];
 
 const ProjectsSection = () => {
